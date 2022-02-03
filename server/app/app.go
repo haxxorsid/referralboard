ae6cb34f664d05292840773fad05f3b6b2095280
haxxorsid <s166865h@gmail.com>
2022-02-02 22:26:07 -0500

    Fixed code and added base path /api

--- server/app/app.go
+++ server/app/app.go
@@ -1,15 +1,19 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"github.com/haxxorsid/referralboard-private/server/handler"
-	"github.com/haxxorsid/referralboard-private/server/config"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/haxxorsid/referralboard/server/config"
+	"github.com/haxxorsid/referralboard/server/models"
+	"github.com/haxxorsid/referralboard/server/services"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
-	"github.com/gorilla/mux"
 )
 
 // App has router and db instances
@@ -20,6 +24,7 @@ type App struct {
 
 // App initialize with predefined configuration
 func (a *App) Initialize(config *config.Config) {
+	// fmt.Println(config.DB.Host, config.DB.Password)
 	dbURI := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
 		config.DB.Host,
 		config.DB.User,
@@ -29,61 +34,128 @@ func (a *App) Initialize(config *config.Config) {
 		config.DB.Sslmode,
 		config.DB.TimeZone)
 
-		database, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{
-			NamingStrategy: schema.NamingStrategy{
-				TablePrefix:   "app.", // schema name
-				SingularTable: false,
-			}})
-	
-		if err != nil {
-			panic("Failed to connect to database!")
-		}
-	
-		a.DB = database
-		a.Router = mux.NewRouter()
-		a.setRouters()
+	database, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "app.", // schema name
+			SingularTable: false,
+		}})
+
+	CheckError(err)
+	fmt.Println("Connected to db")
+
+	a.DB = database
+	a.Router = mux.NewRouter().PathPrefix("/api").Subrouter()
+	a.setRouters()
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 // Set all required routers
 func (a *App) setRouters() {
 	// Routing for handling the projects
-	a.Get("/cards", a.GetAllCards)
-	a.Options("/cards", a.HandleOptions)
+
+	// Routes for user
+	a.Router.HandleFunc("/users", a.GetAllUsers).Methods("GET", "OPTIONS")
+	a.Router.HandleFunc("/users/id/{id}", a.GetUserById).Methods("GET", "OPTIONS")
+	a.Router.HandleFunc("/users/email/{email}", a.GetUserByEmail).Methods("GET", "OPTIONS")
+	a.Router.HandleFunc("/users/newuser", a.AddUser).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/users/{id}", a.UpdateUserById).Methods("PUT", "OPTIONS")
+
+	// Routes for Posts
+	a.Router.HandleFunc("/posts", a.GetAllPosts).Methods("GET", "OPTIONS")
+	a.Router.HandleFunc("/posts/newpost", a.AddPost).Methods("POST", "OPTIONS")
+	a.Router.HandleFunc("/posts/{id}", a.DeletePost).Methods("DELETE", "OPTIONS")
 }
 
-// Wrap the router for GET method
-func (a *App) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
-	a.Router.HandleFunc(path, f).Methods("GET")
+// Wrap the get all post method
+func (a *App) GetAllPosts(w http.ResponseWriter, r *http.Request) {
+	services.GetAllPosts(a.DB, w, r)
 }
 
-// Wrap the router for POST method
-func (a *App) Post(path string, f func(w http.ResponseWriter, r *http.Request)) {
-	a.Router.HandleFunc(path, f).Methods("POST")
+// Wrap the add new post method
+func (a *App) AddPost(w http.ResponseWriter, r *http.Request) {
+	var post models.Post
+	err := json.NewDecoder(r.Body).Decode(&post)
+	CheckError(err)
+	newPost, er := services.AddPost(a.DB, post)
+	if er != nil {
+		services.RespondError(w, http.StatusBadRequest, "Error occured while trying to add post")
+	} else {
+		services.RespondJSON(w, http.StatusOK, newPost)
+	}
 }
 
-// Wrap the router for PUT method
-func (a *App) Put(path string, f func(w http.ResponseWriter, r *http.Request)) {
-	a.Router.HandleFunc(path, f).Methods("PUT")
+// Wrap the delete post method
+func (a *App) DeletePost(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	CheckError(err)
+	post, er := services.DeletePost(a.DB, id)
+	if er != nil {
+		services.RespondError(w, http.StatusBadRequest, "Error while trying to delete post")
+	} else {
+		services.RespondJSON(w, http.StatusOK, post)
+	}
 }
 
-// Wrap the router for DELETE method
-func (a *App) Delete(path string, f func(w http.ResponseWriter, r *http.Request)) {
-	a.Router.HandleFunc(path, f).Methods("DELETE")
+// Wrap the GET all Users method
+func (a *App) GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	services.GetAllUsers(a.DB, w, r)
 }
 
-// Wrap the router for DELETE method
-func (a *App) Options(path string, f func(w http.ResponseWriter, r *http.Request)) {
-	a.Router.HandleFunc(path, f).Methods("OPTIONS")
+// Wrap the GET  User by Id method
+func (a *App) GetUserById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	CheckError(err)
+	user, er := services.GetUserById(a.DB, id)
+	if er != nil {
+		services.RespondError(w, http.StatusBadRequest, "Error occured while trying to fetch user")
+	} else {
+		services.RespondJSON(w, http.StatusOK, user)
+	}
 }
 
-// Handlers to get Posts Data
-func (a *App) GetAllCards(w http.ResponseWriter, r *http.Request) {
-	handler.GetAllCards(a.DB, w, r)
+// Wrap the GET User by email method
+func (a *App) GetUserByEmail(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	user, err := services.GetUserByEmail(a.DB, params["email"])
+	if err != nil {
+		services.RespondError(w, http.StatusBadRequest, "Error occured while trying to fetch user")
+	} else {
+		services.RespondJSON(w, http.StatusOK, user)
+	}
 }
 
-// Handler for options
-func (a *App) HandleOptions(w http.ResponseWriter, r *http.Request) {
-	handler.RespondJSON(w, http.StatusOK, "all good")
+// Wrap the POST User method
+func (a *App) AddUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	CheckError(err)
+	newUser, er := services.AddUser(a.DB, user)
+	if er != nil {
+		services.RespondError(w, http.StatusBadRequest, "Error occured while trying to add user")
+	} else {
+		services.RespondJSON(w, http.StatusOK, newUser)
+	}
+}
+
+// Wrap the update user method
+func (a *App) UpdateUserById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	CheckError(err)
+	userBody := json.NewDecoder(r.Body)
+	user, er := services.UpdateUserById(a.DB, w, userBody, id)
+	if er != nil {
+		services.RespondError(w, http.StatusBadRequest, "Error occured while trying to update user")
+	} else {
+		services.RespondJSON(w, http.StatusOK, user)
+	}
 }
 
 // Run the app on it's router

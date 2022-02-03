6cadff550c010e0b698d0cf5f3187ea4d842b701
haxxorsid <s166865h@gmail.com>
2022-02-03 18:34:58 -0500

    Replaced cards with post

--- client/src/components/TempHome.tsx
+++ client/src/components/TempHome.tsx
@@ -1,11 +1,11 @@
-import { cardType } from '../types';
+import { postType } from '../types';
 
-export default function TempHome(props: { cards: cardType[]; }) {
+export default function TempHome(props: { posts: postType[]; }) {
     return (
       <div>
         <h2>Home</h2>
-        {props.cards &&
-                props.cards.map(card => <div key={card.cardId}>{card.title}</div>)
+        {props.posts &&
+                props.posts.map(post => <div key={post.postId}>{post.message} {post.resume} {post.jobLink}</div>)
             }
       </div>
     );

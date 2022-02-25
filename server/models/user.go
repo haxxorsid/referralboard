9932bb4a9c7cbd20c69930fd7c0b5e407215252e
haxxorsid <s166865h@gmail.com>
2022-02-25 12:28:21 -0500

    Updated backend for login and edit-profile

--- server/models/user.go
+++ server/models/user.go
@@ -5,13 +5,32 @@ type User struct {
 	FirstName           string `json:"firstName" gorm:"column:first_name;not null;default:null" validate:"required"`
 	LastName            string `json:"lastName" gorm:"column:last_name;not null;default:null" validate:"required"`
 	CurrentLocation     string `json:"currentLocation" gorm:"column:current_location;not null;default:null" validate:"required"`
-	CurrentCompanyId    uint   `json:"currentCompanyId" gorm:"column:current_company_id;default:null"`
+	CurrentCompanyId    int   `json:"currentCompanyId" gorm:"column:current_company_id;default:null"`
 	CurrentCompanyName  string `json:"currentCompanyName" gorm:"column:current_company_name;not null;default:null" validate:"required"`
 	CurrentPosition     string `json:"currentPosition" gorm:"column:current_position;not null;default:null" validate:"required"`
 	School              string `json:"school" gorm:"column:school;not null;default:null" validate:"required"`
-	YearsOfExperienceId uint   `json:"yearsOfExperienceId" gorm:"column:years_of_experience_id;not null;default:null" validate:"required"`
+	YearsOfExperienceId int   `json:"yearsOfExperienceId" gorm:"column:years_of_experience_id;not null;default:null" validate:"required"`
 	Email               string `json:"email" gorm:"column:email;uniqueIndex:uq_users_email;not null;default:null" validate:"required,email"`
 	Password            string `json:"password" gorm:"column:password_hash;not null;default:null" validate:"required"`
-	Verified            bool   `json:"verified" gorm:"column:verified;not null;default:false"`
-	Posts               []Post `gorm:"foreignKey:user_id;references:Id"`
+	Verified            *bool   `json:"verified" gorm:"column:verified;not null;default:false"`
+	Posts               []Post `json:"posts,omitempty" gorm:"foreignKey:user_id;references:Id"`
+}
+
+type UserProfile struct {
+	FirstName           string `json:"firstName" validate:"required"`
+	LastName            string `json:"lastName" validate:"required"`
+	CurrentLocation     string `json:"currentLocation" validate:"required"`
+	CurrentCompanyName  string `json:"currentCompanyName" validate:"required"`
+	CurrentPosition     string `json:"currentPosition" validate:"required"`
+	School              string `json:"school" validate:"required"`
+	YearsOfExperienceId int    `json:"yearsOfExperienceId" validate:"required"`	
+}
+
+type UserEmail struct {
+	Email               string `json:"email" validate:"required,email"`
+}
+
+type UserPassword struct {
+	CurrentPassword     string `json:"currentPassword" validate:"required"`
+	NewPassword         string `json:"newPassword" validate:"required"`
 }
--- server/services/companies.go
+++ server/services/companies.go
@@ -3,7 +3,7 @@ package services
 import (
 	"net/http"
 
-	"github.com/haxxorsid/referralboard/server/models"
+	"github.com/haxxorsid/referralboard-private/server/models"
 	"gorm.io/gorm"
 )
 
@@ -14,9 +14,18 @@ func GetAllCompanies(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 	RespondJSON(w, http.StatusOK, company)
 }
 
+
 // Fetch Company by Domain
 func GetCompanyByDomain(db *gorm.DB, domain string) (models.Company, error) {
 	var company models.Company
 	err := db.Where(&models.Company{Domain: domain}).First(&company).Error
+	return company, err
+}
+
+
+// Fetch Company by id
+func GetCompanyById(db *gorm.DB, id int) (models.Company, error) {
+	var company models.Company
+	err := db.Where(&models.Company{Id: id}).First(&company).Error
 	return company, err
 }

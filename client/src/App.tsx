6bb2d3fc5789acb112dc1ec1014923bd2ad54ce2
Vineetkhadloya <vineetkhadloya@gmail.com>
2022-03-01 02:45:31 -0500

    Updated Login and Register Page Front End

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,73 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
-import TempHome from './components/TempHome';
 import './App.css';
-import { useEffect, useState } from 'react';
-import { getPosts } from './common/apiService';
-import { postType } from './types';
+import { useEffect } from 'react';
+import { validateLogin } from './common/apiService';
 import Home from './components/Home/Home';
 import EditProfile from './components/EditProfile/EditProfile';
+import { AuthProvider, useAuth } from './common/auth';
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  let auth = useAuth();
+  const location = useLocation();
+  let from = (location.state as any)?.from?.pathname || "/login";
+
+  useEffect(() => {
+        validateLogin().then(() => {
+          auth.logInValidated(true);
+        }).catch(() => {
+          auth.logInValidated(false);
+        }); 
+  }, [location.key]);
+
+  if (!auth.loggedIn) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to={from} state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+
+function RequireNoAuth({ children }: { children: JSX.Element }) {
+  let auth = useAuth();
+  let location = useLocation();
+  let from = (location.state as any)?.from?.pathname || "/";
+
+  useEffect(() => {
+        validateLogin().then(() => {
+          auth.logInValidated(true);
+        }).catch(() => {
+          auth.logInValidated(false);
+        });
+  }, [location.key]);
+
+  if (auth.loggedIn) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to={from} state={{ from: location }} replace />;
+  }
+
+  return children;
+}
 
 export default function App() {
-  const [posts, setPosts] = useState<postType[]>([]);
-  const [error, setError] = useState(null);
-
-  // get cards, set cards if success, otherwise set error
-  useEffect(() => {
-    getPosts()
-      .then((res) => {
-        setPosts(res);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  }, []);
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/home' element={<TempHome posts={posts} />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/edit-profile' element={<EditProfile />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<RequireAuth><Home /></RequireAuth>} />
+          <Route path='/login' element={<RequireNoAuth><Login /></RequireNoAuth>} />
+          <Route path='/register' element={<RequireNoAuth><Register /></RequireNoAuth>} />
+          <Route path='/edit-profile' element={<RequireAuth><EditProfile /></RequireAuth>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }

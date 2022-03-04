abf09ba8406cf412808bad34650a0d62123c39be
haxxorsid <s166865h@gmail.com>
2022-03-04 14:00:40 -0500

    Updated SQL Code

--- db/create_table_posts.sql
+++ db/create_table_posts.sql
@@ -12,7 +12,15 @@ CREATE TABLE IF NOT EXISTS app.posts
     resume text COLLATE pg_catalog."default",
     job_link text COLLATE pg_catalog."default",
     created_at timestamp with time zone,
-    CONSTRAINT posts_pkey PRIMARY KEY (post_id)
+    CONSTRAINT posts_pkey PRIMARY KEY (post_id),
+    CONSTRAINT fk_app_posts_company FOREIGN KEY (target_company_id)
+        REFERENCES app.companies (company_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_app_users_posts FOREIGN KEY (user_id)
+        REFERENCES app.users (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;

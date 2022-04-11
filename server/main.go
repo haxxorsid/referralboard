d8409bdf16bcd1afa3ceb5f74f26bcd7e9ef17ab
bahetianjali <bahetianjali.10@gmail.com>
2022-04-11 02:38:39 -0400

    added postgres database flag

--- server/main.go
+++ server/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"flag"
 	"github.com/haxxorsid/referralboard/server/app"
 	"github.com/haxxorsid/referralboard/server/config"
 )
 
 func main() {
 	config := config.GetConfig()
+	pgDb := flag.Bool("pgdb", true, "connect to a postgres database")
 
 	app := &app.App{}
-	app.Initialize(config, true)
+	app.Initialize(config, *pgDb)
 	app.SetUpDB()
 	app.Run(":5555")
 }
